a3b02dd165bdfd024bd1411a88d9e569f24e0390
Genadi Samokovarov <gsamokovarov@gmail.com>
2012-08-11 22:57:16 +0300

    Proper support for pages with multiple frames
    Also added an icon (based on Tango's search icon) and bumped the version
    to 0.9. I simply don't have any more requirements for this one. If
    everythin runs fine for a while, will bump it to 1.0.

--- data/holmes-debug.js
+++ data/holmes-debug.js
@@ -1,4 +1,21 @@
+// Collects all of the subframes of a window.
+const collectFrames = function(window) {
+  let result = [window];
+
+  for (let i = 0, len = window.frames.length; i < len; i++) {
+    // For some reason `result.concat` just won't do.
+    result = [].concat(result, collectFrames(window.frames[i]));
+  }
+
+  return result;
+};
+
 self.on('click', function() {
-  $('html').toggleClass('holmes-debug');
+  let frames = collectFrames(unsafeWindow.top);
+
+  for (let i = 0, len = frames.length; i < len; i++) {
+    $('html', frames[i].document).toggleClass('holmes-debug');
+  }
+
   self.postMessage('trigger');
 });
